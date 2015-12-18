df69d214bf54c36f1174e1da4ebe13bad029c2b4
Chanakya <chanakya@localhost.localdomain>
2015-12-18 10:34:38 +0530

    first commit

---
+++ invoice.js
@@ -0,0 +1,65 @@
+var https   = require("https");
+var fs      = require("fs");
+
+function generateInvoice(invoice, filename, success, error) {
+    var postData = JSON.stringify(invoice);
+    var options = {
+        hostname  : "invoice-generator.com",
+        port      : 443,
+        path      : "/",
+        method    : "POST",
+        headers   : {
+            "Content-Type": "application/json",
+            "Content-Length": postData.length
+        }
+    };
+
+    var file = fs.createWriteStream(filename);
+
+    var req = https.request(options, function(res) {
+        res.on('data', function(chunk) {
+            file.write(chunk);
+        })
+        .on('end', function() {
+            file.end();
+
+            if (typeof success === 'function') {
+                success();
+            }
+        });
+    });
+    req.write(postData);
+    req.end();
+
+    if (typeof error === 'function') {
+        req.on('error', error);
+    }
+}
+
+var invoice = {
+    logo: "http://invoiced.com/img/logo-invoice.png",
+    from: "Jai Narayan SIngh\nPayagpur\nBahraich, UP",
+    to: "Dhruv Narayan Singh",
+    currency: "INR",
+    number: "INV-0001",
+    payment_terms: "Auto-Billed - Do Not Pay",
+    items: [
+        {
+            name: "Subscription to Starter",
+            quantity: 1,
+            unit_cost: 50
+        }
+    ],
+    fields: {
+        tax: "%"
+    },
+    tax: 5,
+    notes: "Thanks for being an awesome customer!",
+    terms: "No need to submit payment. You will be auto-billed for this invoice."
+};
+
+generateInvoice(invoice, 'invoice_new.pdf', function() {
+    console.log("Saved invoice to invoice.pdf");
+			}, function(error) {
+    console.error(error);
+});
